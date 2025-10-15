1682c7289d8aabbcbfcc13da68d7cecb18907b83
Aymeric DE LANGE <aymericdelange80@gmail.com>
2025-10-15 21:10:40 +0200

    feat: integrate GSAP and Lenis for smooth scrolling in app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import gsap from "gsap";
+import { watch, watchEffect } from "vue";
+
+const lenis = useLenis((lenis) => {
+  console.log("page callback", lenis);
+});
+
+watch(
+  lenis,
+  (lenis) => {
+    console.log("page", lenis);
+  },
+  { immediate: true },
+);
+
+const lenisRef = useTemplateRef("lenisRef");
+
+watchEffect((onInvalidate) => {
+  function update(time: number) {
+    lenisRef.value?.lenis?.raf(time * 1000);
+  }
+  gsap.ticker.add(update);
+
+  onInvalidate(() => {
+    gsap.ticker.remove(update);
+  });
+});
+</script>
 
 <template>
   <div>
     <div class="lcd"></div>
     <Header />
+    <VueLenis root :options="{ autoRaf: false }" ref="lenisRef" />
     <NuxtPage />
     <Footer />
   </div>

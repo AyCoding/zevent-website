693090c5377adbc00abf01376d4609128ddcc20e
Aymeric DE LANGE <aymericdelange80@gmail.com>
2025-10-16 14:33:30 +0200

    update lenis error on static generate and scroll reset to top for each navigation

--- app/app.vue
+++ app/app.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
 import gsap from "gsap";
 import { watch, watchEffect } from "vue";
+import { useRoute } from "vue-router";
 
-const lenis = useLenis((lenis) => {
-  console.log("page callback", lenis);
-});
+if (typeof window !== "undefined") {
+  const route = useRoute();
 
-watch(
-  lenis,
-  (lenis) => {
-    console.log("page", lenis);
-  },
-  { immediate: true },
-);
-
-const lenisRef = useTemplateRef("lenisRef");
-
-watchEffect((onInvalidate) => {
-  function update(time: number) {
-    lenisRef.value?.lenis?.raf(time * 1000);
-  }
-  gsap.ticker.add(update);
-
-  onInvalidate(() => {
-    gsap.ticker.remove(update);
+  const lenis = useLenis((lenis) => {
+    console.log("page callback", lenis);
   });
-});
+
+  watch(
+    lenis,
+    (lenis) => {
+      console.log("page", lenis);
+    },
+    { immediate: true },
+  );
+
+  const lenisRef = useTemplateRef("lenisRef");
+
+  watch(
+    () => route.fullPath,
+    () => {
+      const instance = lenisRef.value?.lenis ?? (lenis as any)?.value;
+      instance?.scrollTo?.(0, { immediate: true });
+    },
+  );
+
+  watchEffect((onInvalidate) => {
+    function update(time: number) {
+      lenisRef.value?.lenis?.raf(time * 1000);
+    }
+    gsap.ticker.add(update);
+
+    onInvalidate(() => {
+      gsap.ticker.remove(update);
+    });
+  });
+}
 </script>
 
 <template>

10ee8a213d0ae38ffb55a101c128d6430c37a9d0
Aymeric DE LANGE <aymericdelange80@gmail.com>
2025-10-16 21:09:23 +0200

    feat: update Nightline section with new content and links

--- app/components/Associations/Nightline.vue
+++ app/components/Associations/Nightline.vue
@@ -4,70 +4,60 @@
   <div class="pt-[36px] pb-40">
     <div class="flex bg-[#16161B] mx-auto max-w-screen-xl p-6 rounded-2xl">
       <div class="flex flex-col gap-8 p-11 max-w-[355px] w-full">
-        <img src="/logos/aidants.png" alt="" />
+        <img src="/logos/nightline.webp" alt="" />
 
-        <CTAMain to="https://www.aidants.fr/" target="_blank">
+        <CTAMain to="https://www.nightline.fr/" target="_blank">
           Site officiel
         </CTAMain>
       </div>
       <div class="p-11">
         <div class="flex flex-col gap-6">
-          <h2 class="text-[32px] font-semibold text-pretty">
-            Association Française des Aidants
-          </h2>
+          <h2 class="text-[32px] font-semibold text-pretty">Nightline</h2>
           <div class="flex flex-col gap-4">
             <p class="leading-7">
-              Depuis sa création en 2003, l’Association Française des aidants
-              écoute et porte la parole des proches aidants de personnes
-              vulnérables par l’âge, la maladie ou le handicap. Elle agit pour
-              qu’ils soient pris en considération dans leur juste rôle et à leur
-              juste place.
+              Nightline France est une association engagée pour la santé mentale
+              des jeunes et des étudiants, à travers des actions de soutien,
+              d’orientation et de sensibilisation.
             </p>
-            <p class="leading-7 underline">Une action autour de 3 piliers :</p>
-            <div class="leading-7">
-              <ul class="pl-4 list-disc">
-                <li>
-                  agir pour qu’aider son proche devienne un choix librement
-                  consenti et accompagné par la société.
-                </li>
-                <li>
-                  organiser des dispositifs sur les territoires, comme l’Atelier
-                  “Comprendre pour agir” et les “Cafés des aidants®”, avec les
-                  aidants et les professionnels.
-                </li>
-                <li>
-                  former à la fois les professionnels du secteur social,
-                  médico-social et sanitaire et sensibiliser les acteurs de
-                  l’entreprise à travers des formations et conférences…
-                </li>
-              </ul>
-            </div>
-          </div>
-          <h2 class="text-[32px] font-semibold text-pretty pt-20">
-            A quoi serviront les fonds récoltés ?
-          </h2>
-          <div class="flex flex-col gap-4">
-            <div class="leading-7">
-              <ul class="pl-4 list-disc">
-                <li>
-                  à créer 60 nouveaux “Cafés des aidants ®” sur de nouveaux
-                  territoires et ainsi qu’à accompagner plus de 1 000 proches
-                  aidants ;
-                </li>
-                <li>
-                  à déployer 70 nouveaux ateliers de formation pour les aidants
-                  afin d’accompagner près de 500 proches aidants ;
-                </li>
-                <li>
-                  à lancer 10 “Ateliers Santé” afin que 200 proches aidants
-                  puissent davantage prendre soin de leur santé mentale et
-                  physique ;
-                </li>
-                <li>
-                  à sensibiliser et former plus de 5 000 professionnels au
-                  repérage et à l’accompagnement de proches aidants.
-                </li>
-              </ul>
+            <p class="leading-7">
+              Sa mission est d’offrir des ressources accessibles et adaptées aux
+              jeunes, conçues par et pour eux, avec l’appui de professionnels.
+              Depuis sa création, l’association agit concrètement pour libérer
+              la parole et réduire l’isolement des jeunes, souvent confrontés à
+              des difficultés psychologiques.
+            </p>
+            <p class="leading-7">
+              Nightline est aujourd’hui présente dans 9 régions en France
+              métropolitaine et ultramarine. Chaque action vise à faire reculer
+              les tabous et à permettre à chaque jeune de se sentir écouté et
+              soutenu.
+            </p>
+
+            <h2 class="text-[32px] font-semibold text-pretty pt-20">
+              A quoi serviront les fonds récoltés ?
+            </h2>
+            <div class="flex flex-col gap-4">
+              <p class="leading-7">Vos dons permettront à Nightline :</p>
+              <div class="leading-7">
+                <ul class="pl-4 list-disc">
+                  <li>
+                    d’ouvrir de nouvelles lignes d’écoute dans des territoires
+                    encore non couverts (+ 5 000 appels pris en charge)
+                  </li>
+                  <li>de former davantage de bénévoles (+ 200)</li>
+                  <li>de développer des outils de prévention innovants</li>
+                  <li>
+                    et de sensibiliser toujours plus d’étudiants et de jeunes
+                    aux enjeux de santé mentale (+ 10 000).
+                  </li>
+                </ul>
+              </div>
+              <p class="leading-7">
+                Grâce à votre soutien, Nightline pourra poursuivre son
+                engagement en France pour que chaque jeune puisse trouver une
+                oreille attentive et des ressources adaptées, quel que soit son
+                parcours.
+              </p>
             </div>
           </div>
         </div>

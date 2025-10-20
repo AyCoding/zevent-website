ceca43f8cef8d14a185ceb194726668a419ddc9d
Aymeric DE LANGE <aymericdelange80@gmail.com>
2025-10-20 20:32:46 +0200

    refactor: remove scroll handling with lenis and gsap integration from app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,51 +1,9 @@
-<script setup lang="ts">
-import gsap from "gsap";
-import { watch, watchEffect } from "vue";
-import { useRoute } from "vue-router";
-
-if (typeof window !== "undefined") {
-  const route = useRoute();
-
-  const lenis = useLenis((lenis) => {
-    console.log("page callback", lenis);
-  });
-
-  watch(
-    lenis,
-    (lenis) => {
-      console.log("page", lenis);
-    },
-    { immediate: true },
-  );
-
-  const lenisRef = useTemplateRef("lenisRef");
-
-  watch(
-    () => route.fullPath,
-    () => {
-      const instance = lenisRef.value?.lenis ?? (lenis as any)?.value;
-      instance?.scrollTo?.(0, { immediate: true });
-    },
-  );
-
-  watchEffect((onInvalidate) => {
-    function update(time: number) {
-      lenisRef.value?.lenis?.raf(time * 1000);
-    }
-    gsap.ticker.add(update);
-
-    onInvalidate(() => {
-      gsap.ticker.remove(update);
-    });
-  });
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <div class="lcd"></div>
     <Header />
-    <VueLenis root :options="{ autoRaf: false }" ref="lenisRef" />
     <NuxtPage />
     <Footer />
   </div>
